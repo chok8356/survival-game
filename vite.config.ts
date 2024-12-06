4051cccbbf6d537206eabbb55d534ca9f325f311
Nikolai <chok8356@gmail.com>
2024-12-07 02:17:56 +0300

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,22 @@
-export default {}
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+
+  let base = '/'
+
+  if (isProduction) {
+    base = '/vue3-flowchart/'
+  }
+
+  return {
+    base,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
+})
